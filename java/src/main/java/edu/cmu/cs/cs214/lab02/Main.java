ab12e56d42a3c2f05b20fb8d13b8976798417389
Jargalbaatar-NFB <jbaatar501@gmail.com>
2025-04-18 18:53:30 +0800

    badhsfhgfhdsa

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02;
-
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2.0);
+        Shape square = new Square(4.0);
+        Shape rectangle = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
-
-        renderer.draw();
+        Renderer circleRenderer = new Renderer(circle);
+        Renderer squareRenderer = new Renderer(square);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+//aaqwhgjfq fgit JFGASGHFGHAGKFGKAGFAGFHGALGFGAKFGKAGFKGAOFGOYWQEGFLGAOYEQGOAY
+        circleRenderer.draw();
+        squareRenderer.draw();
+        rectangleRenderer.draw();
     }
 }
+
+
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -1,19 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
-
-import edu.cmu.cs.cs214.lab02.shapes.*;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    public Shape shape;
+
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
+        double area = shape.getArea();
 
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
 }
+
+
